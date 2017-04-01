3e8d96936ba16d1398cab22bba7c8d17e8a625c1
Aaron Berk <aaron.riot1@gmail.com>
2017-04-01 15:19:58 -0700

    close file protocol when done

--- python/ProxDenoisSPGL1HistData.py
+++ python/ProxDenoisSPGL1HistData.py
@@ -9,10 +9,11 @@ def main(logNmax, filename, logfile=sys.stdout):
                   'sqNormZ': pdmse.pdmse_batch(logNmax,
                                                theta='sqNormZ',
                                                verbose=logfile)}
-    fp = open(logfile, 'a', encoding='utf-8')
-    print('\nRuns complete. Saving dict to json...', file=fp, end='')
+    with open(logfile, 'a', encoding='utf-8') as fp:
+        print('\nRuns complete. Saving dict to json...', file=fp, end='')
     jsonWrite.dict(filename, pdmse_dict)
-    print('complete!', file=fp)
+    with open(logfile, 'a', encoding='utf-8') as fp:
+        print('complete!', file=fp)
     return
 
 if __name__ == "__main__":
